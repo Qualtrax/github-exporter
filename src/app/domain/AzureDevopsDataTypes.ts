b385289da1000d6c21d6e36512655ac417271d58
Joey Bayes <joseph.w.bayes@outlook.com>
2020-09-29 16:20:39 -0400

    Updated azure devops csv export based on tests.

--- src/app/domain/AzureDevopsDataTypes.ts
+++ src/app/domain/AzureDevopsDataTypes.ts
@@ -2,36 +2,38 @@ import { Strings } from 'tsbase';
 import { Issue } from './GitHubDataTypes';
 
 export const AzureWorkItemFields = [
-  'Work Item Type', 'Title', 'Description', 'Tags','Created Date', 'Closed Date', 'Discussion'
+  'Work Item Type', 'Title', 'Description', 'Tags','Created Date'
 ];
 
 export enum WorkItemType {
-  UserStory = 'User Story',
-  CodeReview = 'Code Review Request'
+  Issue = 'Issue'
 }
 
 export class AzureDevopsWorkItem {
-  public workItemType = WorkItemType.UserStory;
+  public workItemType = WorkItemType.Issue;
   public title = Strings.Empty;
   public description = Strings.Empty;
   public tags = Strings.Empty;
   public createdDate = Strings.Empty;
-  public closedDate = Strings.Empty;
-  public discussion = Strings.Empty;
 
   constructor(issue?: Issue) {
     if (issue) {
-      this.title = this.minified(issue.title),
-      this.description = this.minified(issue.bodyHTML);
+      const commentsStringMap = issue.comments.nodes.map(n => /*html*/
+        (`<h3>Author: ${n.author.login} | Date: ${n.createdAt}</h3>${this.minified(n.bodyHTML)}`)).toString();
+
+      this.title = this.minified(issue.title);
+      this.description =
+        `<h2>GitHub Description</h2>${this.minified(issue.bodyHTML)}<h2>GitHub Comments</h2>${commentsStringMap}`;
       this.tags = issue.labels.nodes.map(n => n.name).toString();
       this.createdDate = issue.createdAt;
-      this.closedDate = issue.closedAt;
-      this.discussion = this.minified(issue.comments.nodes.map(n => /*html*/
-        (`<p>Author: ${n.author.login}</p><p>Date: ${n.createdAt}</p>${n.bodyHTML}`)).toString());
     }
   }
 
   private minified(string: string): string {
-    return JSON.stringify(string.replace(/,/g, '&#44;')).slice(1, -1);
+    return JSON.stringify(string)
+      .slice(1, -1)
+      .replace(/,/g, '&#44;')
+      .replace(/\"/g, '&quot;')
+      .replace(/\\n/g, Strings.Empty);
   }
 }
